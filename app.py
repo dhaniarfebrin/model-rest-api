bf08fba54fe1341972f0befd05aca15ba4866a3d
dhaniarfebrin <dhaniarfebrinwahyudi@gmail.com>
2024-12-05 23:03:13 +0700

    feat: new model

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 with open('svm_model_rbf.pkl', 'rb') as f:
     model = pickle.load(f)
 
-with open('tfidf_vectorizer.pkl', 'rb') as f:
+with open('tf-idf-vectorizer.pkl', 'rb') as f:
     tfidf = pickle.load(f)
 
 @app.route("/")
@@ -24,16 +24,40 @@ def hello():
 @app.route('/predict', methods=['POST'])
 @cross_origin(origin="*",headers=['Content-Type'])
 def predict():
-    data = request.json['data']
+    data = request.json.get('data')
 
-    # Mengubah teks menjadi representasi TF-IDF
-    text_vector = tfidf.transform([data]).toarray()
+    if not isinstance(data, str):
+        return jsonify({
+            "status": "error",
+            "code": 400,
+            "message": "Invalid input: 'data' must be a string"
+        }), 400
 
-    # Make the prediction
-    prediction = model.predict(text_vector)
-    prediction_str = str(prediction[0])
+    try:
+        # Mengubah teks menjadi representasi TF-IDF
+        text_vector = tfidf.transform([data]).toarray()
 
-    return jsonify({'prediction': prediction_str})
+        # Make the prediction
+        prediction = model.predict(text_vector)
+        prediction_str = str(prediction[0])
+
+        probability = model.predict_proba(text_vector)
+        probability_str = str(probability[0][0])
+
+        return jsonify({
+            "status": "success",
+            "code": 200,
+            "data": {
+                'prediction': prediction_str,
+                'probability': probability_str,
+                }
+            }), 200
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "code": 500,
+            "message": str(e)
+            }), 500
 
 if __name__ == "__main__":
     app.run()
